51208128f5660d4848e7e44d6b79f03ea53c16c3
Repooc <tjcooper1982@gmail.com>
2022-03-05 01:04:19 -0500

    help/information section

--- ElvUI_ActionBarMasks/Options.lua
+++ ElvUI_ActionBarMasks/Options.lua
@@ -1,7 +1,33 @@
 local E, L, _, P, _ = unpack(ElvUI)
 local ABM = E:GetModule('ElvUI_ActionBarMasks')
+local ABMCL = E:GetModule('ABM-Changelog')
 local AB = E.ActionBars
 local ACH = E.Libs.ACH
+
+local DONATORS = {
+	'None to be displayed at this time.',
+}
+
+local DEVELOPERS = {
+	'|cff82B4ffEltreum|r',
+}
+
+local TESTERS = {
+	'|cffeb9f24Tukui Community|r',
+	'Linaori',
+}
+
+local function SortList(a, b)
+	return E:StripString(a) < E:StripString(b)
+end
+
+sort(DONATORS, SortList)
+sort(DEVELOPERS, SortList)
+sort(TESTERS, SortList)
+
+local DONATOR_STRING = table.concat(DONATORS, '|n')
+local DEVELOPER_STRING = table.concat(DEVELOPERS, '|n')
+local TESTER_STRING = table.concat(TESTERS, '|n')
 
 local function GetBorderValues()
 	local maskDB = ABM:GetMaskDB()
@@ -50,6 +76,43 @@ local function configTable()
 		ABM:UpdateOptions()
 	end)
 	Proc.args.procStyle = ACH:Select(L["Style"], nil, 4, {pixel = 'Pixel', solid = 'Solid'})
+
+	local Help = ACH:Group(L["Help"], nil, 99, nil, nil, nil, false)
+	rab.args.help = Help
+
+	local Support = ACH:Group(L["Support"], nil, 1)
+	Help.args.support = Support
+	Support.inline = true
+	Support.args.wago = ACH:Execute(L["Wago Page"], nil, 1, function() E:StaticPopup_Show('ELVUI_EDITBOX', nil, nil, 'https://addons.wago.io/addons/elvui-actionbarmasks') end, nil, nil, 140)
+	Support.args.curse = ACH:Execute(L["Curseforge Page"], nil, 1, function() E:StaticPopup_Show('ELVUI_EDITBOX', nil, nil, 'https://www.curseforge.com/wow/addons/actionbar-masks-elvui-plugin') end, nil, nil, 140)
+	Support.args.git = ACH:Execute(L["Ticket Tracker"], nil, 2, function() E:StaticPopup_Show('ELVUI_EDITBOX', nil, nil, 'https://github.com/Repooc/ElvUI_ActionBarMasks/issues') end, nil, nil, 140)
+	Support.args.discord = ACH:Execute(L["Discord"], nil, 3, function() E:StaticPopup_Show('ELVUI_EDITBOX', nil, nil, 'https://discord.gg/') end, nil, nil, 140, nil, nil, true)
+
+	local Download = ACH:Group(L["Download"], nil, 2)
+	Help.args.download = Download
+	Download.inline = true
+	Download.args.development = ACH:Execute(L["Development Version"], L["Link to the latest development version."], 1, function() E:StaticPopup_Show('ELVUI_EDITBOX', nil, nil, 'https://github.com/Repooc/ElvUI_ActionBarMasks/archive/refs/heads/main.zip') end, nil, nil, 140)
+	Download.args.changelog = ACH:Execute(L["Changelog"], nil, 3, function() if ABM_Changelog and ABM_Changelog:IsShown() then ABM:Print('ActionBar Masks changelog is already being displayed.') else ABMCL:ToggleChangeLog() end end, nil, nil, 140)
+
+	local Credits = ACH:Group(L["Credits"], nil, 5)
+	Help.args.credits = Credits
+	Credits.inline = true
+	Credits.args.string = ACH:Description(E:TextGradient(L["ABM_CREDITS"], 0.27,0.72,0.86, 0.51,0.36,0.80, 0.69,0.28,0.94, 0.94,0.28,0.63, 1.00,0.51,0.00, 0.27,0.96,0.43), 1, 'medium')
+
+	local Coding = ACH:Group(L["Textures/Coding"], nil, 6)
+	Help.args.coding = Coding
+	Coding.inline = true
+	Coding.args.string = ACH:Description(DEVELOPER_STRING, 1, 'medium')
+
+	local Testers = ACH:Group(L["Help Test Dev Versions"], nil, 7)
+	Help.args.testers = Testers
+	Testers.inline = true
+	Testers.args.string = ACH:Description(TESTER_STRING, 1, 'medium')
+
+	local Donators = ACH:Group(L["Donators"], nil, 8)
+	Help.args.donators = Donators
+	Donators.inline = true
+	Donators.args.string = ACH:Description(DONATOR_STRING, 1, 'medium')
 end
 
 tinsert(ABM.Configs, configTable)
