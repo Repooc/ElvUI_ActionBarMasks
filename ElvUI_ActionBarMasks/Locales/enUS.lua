6886b3eccc61ab518ec7140bd5844aeb45a7c856
Repooc <tjcooper1982@gmail.com>
2022-03-05 02:04:10 -0500

    locales

--- ElvUI_ActionBarMasks/Locales/enUS.lua
+++ ElvUI_ActionBarMasks/Locales/enUS.lua
@@ -1,4 +1,25 @@
 -- English localization file for enUS and enGB.
-local L = ElvUI[1].Libs.ACL:NewLocale("ElvUI", "enUS", true, true)
+local L = ElvUI[1].Libs.ACL:NewLocale('ElvUI', 'enUS', true, true)
 
 L["ABM_CREDITS"] = "Just a want to give credit to The following people for helping me maintain this addon with testing, coding, textures and people who also have helped me through donations. Please note for donation acknowledgments, only the names of people who messaged me or noted in the donation will be listed as you ask (within reason of course)."
+L["Curseforge Page"] = true
+L["Donators"] = true
+L["Help Testing Development Versions"] = true
+L["Help"] = true
+L["Mask & Glow Options"] = true
+L["Mask Shape"] = true
+L["Pulse"] = true
+L["Spin"] = true
+L["Textures/Coding"] = true
+L["Wago Page"] = true
+
+-- Do not translate as they are in ElvUI Locales already
+-- L["Border Options"]
+-- L["Changelog"]
+-- L["Color"]
+-- L["Discord"]
+-- L["Enable"]
+-- L["Glow"]
+-- L["Style"]
+-- L["Support"]
+-- L["Ticket Tracker"]
