c05dd07297df06bd7bf5712fae529bc26568257c
Repooc <tjcooper1982@gmail.com>
2022-03-05 01:02:22 -0500

    locales...why not

---
+++ ElvUI_ActionBarMasks/Locales/enUS.lua
@@ -0,0 +1,4 @@
+-- English localization file for enUS and enGB.
+local L = ElvUI[1].Libs.ACL:NewLocale("ElvUI", "enUS", true, true)
+
+L["ABM_CREDITS"] = "Just a want to give credit to The following people for helping me maintain this addon with testing, coding, textures and people who also have helped me through donations. Please note for donation acknowledgments, only the names of people who messaged me or noted in the donation will be listed as you ask (within reason of course)."
