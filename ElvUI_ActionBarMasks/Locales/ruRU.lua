0a82eea96a35c1c1ff97a5cd86a7f870eca5cd13
ZamestoTV <34053496+Hubbotu@users.noreply.github.com>
2025-04-18 09:49:07 +0300

    Create ruRU.lua

---
+++ ElvUI_ActionBarMasks/Locales/ruRU.lua
@@ -0,0 +1,27 @@
+-- Локализация на русский язык (ruRU) ZamestoTV
+local L = ElvUI[1].Libs.ACL:NewLocale('ElvUI', 'ruRU', true, true)
+
+L["ABM_CREDITS"] = "Хочу выразить благодарность следующим людям за помощь в поддержке этого аддона: тестирование, программирование, текстуры, а также тем, кто помог мне через пожертвования. Обратите внимание, что в списке благодарностей за пожертвования будут указаны только имена тех, кто сообщил мне об этом или оставил заметку при переводе (в разумных пределах, конечно)."
+L["Border / Shadow Options"] = "Настройки границы и тени"
+L["Curseforge Page"] = "Страница на Curseforge"
+L["Donators"] = "Благодарности"
+L["Help Testing Development Versions"] = "Помощь в тестировании версий в разработке"
+L["Help"] = "Помощь"
+L["Mask & Glow Options"] = "Настройки маски и свечения"
+L["Mask Shape"] = "Форма маски"
+L["Pulse"] = "Пульсация"
+L["Spin"] = "Вращение"
+L["Textures/Coding"] = "Текстуры/Программирование"
+L["The lower the setting, the faster. This value represents the time it takes to rotate 360 degrees."] = "Чем ниже значение, тем быстрее. Эта величина представляет время, необходимое для полного оборота на 360 градусов."
+L["Wago Page"] = "Страница на Wago"
+
+-- Не переводить, так как эти строки уже есть в локализации ElvUI
+-- L["Changelog"]
+-- L["Color"]
+-- L["Credits"]
+-- L["Discord"]
+-- L["Enable"]
+-- L["Glow"]
+-- L["Style"]
+-- L["Support"]
+-- L["Ticket Tracker"]
